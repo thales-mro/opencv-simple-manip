062147a8e0d75cf16c43074d0920ea704624a2a0
Thales <thalesteng@gmail.com>
2019-08-20 17:28:53 -0300

    Replacing correct channel (red channel)

--- src/main.py
+++ src/main.py
@@ -51,7 +51,7 @@ def pixelReplacement(image_original, monocA, monocB):
     monocB[(heightB//2)-50:(heightB//2)+50,(widthB//2)-50:(widthB//2)+50] = middleA
     cv2.imwrite('output/o-3-0.jpg', monocB, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
-    image_final[:,:,1] = monocB[:,:,1]
+    image_final[:,:,2] = monocB[:,:,2]
     cv2.imwrite('output/o-3-1.jpg', image_final, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
 
